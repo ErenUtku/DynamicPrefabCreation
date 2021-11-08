8d43bf1e6e564fc80d00abcdb596b930dc1df412
ErenUtku <61837087+ErenUtku@users.noreply.github.com>
2021-11-08 16:03:35 +0300

    Init

---
+++ Assets/AsteroidSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    public Material material;
+    public GameObject asteroid;
+
+    
+    public void CreateAstroid()
+    {
+        asteroid = ProcAsteroid.Clone(this.transform.position);
+        asteroid.GetComponent<MeshRenderer>().sharedMaterial = material;
+    }
+}
